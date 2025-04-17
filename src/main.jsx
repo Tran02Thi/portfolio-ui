d3bc398c2cea9a195c9b1e3db4d1f09c1c574216
ThiTV <ThiTV@vetc.com.vn>
2025-04-18 01:50:02 +0700

    feature: test jenkins

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Layout from './components/Layout/Layout'
-// import Home from './pages/Home/Home'
-// import Blog from './pages/Blogs/Blog'
-// import Projects from './pages/Projects/Projects'
-// import Infor from './pages/Infor/Infor'
-// import Experience from './pages/Experience/Experience'
-// import Contact from './pages/Contact/Contact'
+
 
 import "./assets/styles/global.css";
 
